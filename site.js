fda8fc6056cef735b8ae19f3b8b10b30bf16eff7
Emily Chao <elchao96@gmail.com>
2014-09-12 01:04:18 -0500

    adjusting the routes

--- site.js
+++ site.js
@@ -8,40 +8,40 @@ emilyChaoApp.config(function($routeProvider) {
 			templateUrl : 'pages/home.html',
 			controller : 'mainController'
 		})
-		.when('/', {
+		.when('/resume', {
 			templateUrl : 'pages/resume.html',
 			controller : 'resumeController'
 		})
-		.when('/', {
+		.when('/projects', {
 			templateUrl : 'pages/projects.html',
 			controller : 'projectsController'
 		})
-		.when('/', {
+		.when('/blog', {
 			templateUrl : 'pages/blog.html',
 			controller : 'blogController'
 		})
-		.when('/', {
+		.when('/contact', {
 			templateUrl : 'pages/contact.html',
 			controller : 'contactController'
 		});
 })
 
 emilyChaoApp.controller('mainController', function($scope) {
-	$scope.message = "UIUC CS '17, Miss Possible Web Dev, Part of NCWITAiC, Passionate about Gender Equality, LGBTQ Equality in the Workplace, Loves Coding, Thinking Deeply, Writing and Singing at the Top of My Lungs";
+	$scope.message = 'UIUC CS 2017, Miss Possible Web Dev, Part of NCWITAiC, Passionate about Gender Equality, LGBTQ Equality in the Workplace, Loves Coding, Thinking Deeply, Writing and Singing at the Top of My Lungs';
 });
 
 emilyChaoApp.controller('resumeController', function($scope) {
-	$scope.message = "A Collection of Experiences and Education, A Log of My Ongoing Journey to Becoming A Competent Software Engineer";
+	$scope.message = 'A Collection of Experiences and Education, A Log of My Ongoing Journey to Becoming A Competent Software Engineer';
 });
 
 emilyChaoApp.controller('projectsController', function($scope) {
-	$scope.message = "A Record of Complete Projects, What's on the Pipeline, What's to Come, My Ideas Box";
+	$scope.message = 'A Record of Complete Projects, What's on the Pipeline, What's to Come, My Ideas Box';
 });
 
 emilyChaoApp.controller('blogController', function($scope) {
-	$scope.message = "A Stream of Consciouness with Some Editing, An Output of Reflections and Ideas";
+	$scope.message = 'A Stream of Consciouness with Some Editing, An Output of Reflections and Ideas';
 });
 
 emilyChaoApp.controller('contactController', function($scope) {
-	$scope.message = "All Ways to Reach Me, Excluding Snail Mail";
+	$scope.message = 'All Ways to Reach Me, Excluding Snail Mail';
 });
