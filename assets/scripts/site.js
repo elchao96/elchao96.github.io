451cc48c1ec89b53ea3c704e72ab18e03da0c4e8
Emily Chao <elchao96@gmail.com>
2014-11-21 22:48:33 -0600

    revamping website to be one-page responsive scroll

---
+++ assets/scripts/site.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.scroll-link').on('click', function(event) {
+        event.preventDefault();
+        var sectionID = $(this).attr("data-id");
+        scrollToID('#' + sectionID, 750);
+    });
+});
+
+function scrollToID(id, speed) {
+    var offset = 50; 
+    var targetOffset = $(id).offset().top - offset;
+    $('html,body').animate({scrollTop: targetOffset}, speed);
+}
+
+if (typeof console === "undefined") {
+    console = {log: funtion(){}};}
+}
