133a7aa2991013ccb7f1a1f1483662d937e1bb78
Emily Chao <elchao96@gmail.com>
2014-12-31 22:09:02 -0800

    unsuccessful in trying to get active links to work

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -14,8 +14,44 @@ $(document).ready(function() {
         else {
             $('.navbar').removeClass('navbar-fixed-top');
         }
+    });
+    
+    var liChildren = $("#navLinkList").children();
+    var aChildren = [];
+    for (i = 0; i < liChildren.length; i++) {
+        var as = liChildren[i].children();
+        aChildren.push(as);
+    }
+    var aArray = [];
+    for (i = 0; i < aChildren.length; i++) {
+        aArray.push(aChildren[i].attr('data-id'));
+    }
+    
+    $(window).scroll(function() {
+        var windowPosition = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var documentHeight = $(document).height();
         
+        for (i = 0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var divPosition = $(theID).offset().top;
+            var divHeight = $(theID).height();
+            if (windowPosition >= divPosition && windowPosition < (divPosition + divHeight)) {
+                $("a[data-id='" + theID + "']").addClass('navActiveLink');
+            }
+            else
+            {
+                $("a[data-id='" + theID + "']").removeClass('navActiveLink');
+            }
+        }
         
+        if(windowPosition + windowHeight == documentHeight) {
+            if (!$("nav li:last-child a").hasClass("navActiveLink")) {
+                var navActiveCurrent = $(".navActiveLink").attr("data-id");
+                $("a[data-id='" + navActiveCurrent + "']").removeClass("navActiveLink");
+                $("nav li:last-child a").addClass("navActiveLink");
+            }
+        }
     });
 });
 
