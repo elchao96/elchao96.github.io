507e4dfdb39ba625105fa43f92999f2193bad400
Emily Chao <elchao96@live.com>
2016-05-07 20:32:26 -0500

    added some necessary files

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+	gulp.src('styles.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['sass']);
